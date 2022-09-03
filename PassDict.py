61cf606f172d880ee890434d85fe3ef4384f43b1
eronyako <1138345264@qq.com>
2022-09-03 19:14:56 +0800

    v1.0

---
+++ PassDict.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# @Subj: PyCharm
+# @File: PassDict.py
+# @Date: 2022/9/3 17:55
+
+# 读取密码txt
+import os.path
+import sys
+
+
+try:
+    file_name = sys.argv[1]
+except IndexError:
+    print('未拖动文件打开，尝试 passwords.txt')
+    if os.path.isfile('passwords.txt'):
+        file_name = 'passwords.txt'
+    else:
+        raise FileNotFoundError('未找到 passwords.txt')
+
+try:
+    f = open(file_name, 'r', encoding='UTF-8')
+except UnicodeDecodeError:
+    raise UnicodeError('编码不正确，请使用 UTF-8 (without BOM) 编码的文件')
+else:
+    pw_list = f.readlines()
+    f.close()
+
+pw_out = set()
+for v in pw_list:
+    if v == '\n':
+        continue
+    pw_out.add(v)
+
+pw_out = sorted(pw_out)
+
+with open('passwords_out.txt','w',encoding='UTF-8') as f:
+    for v in pw_out:
+        f.write(v)
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # PassDict
-密码字典整理工具
+
+本项目为使用 python3 编写的 密码字典整理去重工具。
+
+## 使用
+本项目可以使用直接使用 PassDict.py 文件（需要安装 Python3 并添加到环境变量），或使用 Releases 的 exe 文件。
+
+所需文件如下：
+
+- 程序主体：PassDict.py / PassDict.exe
+- 密码字典
+
+### 密码字典要求
+
+密码字典文件需要使用 UTF-8 Without BOM 编码。
+
+### 使用源码
+
+拖动密码字典文件到 PassDict.py
+
+直接运行的情况，将会读取当前文件夹下的 passwords.txt
+
+在当前目录运行：
+
+```shell
+python PassDict.py
+```
+
+#### 打包 exe 文件
+
+可使用 pyinstaller 包，用如下命令将会在 `./dist/` 目录下创建 windows 可执行程序：
+
+```shell
+pyinstaller -F --icon=icon.ico PassDict.py
+```
+
+### 使用封装版
+
+拖动密码字典文件到  PassDict.exe
+
+直接运行的情况，将会读取当前文件夹下的 passwords.txt
+
+## 输出
+
+若正常运行后将会在当前文件夹生成去重整理后的 passwords_out.txt 文件。
